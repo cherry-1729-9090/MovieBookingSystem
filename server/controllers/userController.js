b56e5828291ffc814f7b01b53884a1680d8df13b
Devi Charan <145871325+cherry-1729-9090@users.noreply.github.com>
2024-07-17 13:44:28 +0530

    Adde signup and used bcrypt to put in the databae

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,11 +1,66 @@
+const { Message } = require('twilio/lib/twiml/MessagingResponse');
 const User = require('../models/userModel');
+const bcrypt = require('bcryptjs');
 
-const login = async (req,res) => {
-    try{
-        const {email,password} = req.body;
-        const response = await User.findOne({email,password});
-    }
-    catch(error){
+const login = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        const user = await User.findOne({ email:req.body.email });
+        if(! user){
+            res.send({
+                success: false,
+                message: "User not found!",
+            });
+        }
+
+        const isMatch = bcrypt.compareSync(password, user.password);
+        if(! isMatch){
+            res.send({
+                success: false,
+                message: "Invalid password!",
+            });
+        }
+
+        res.send({
+            success:true,
+            message: 'User logged in Scuccessfully!',
+        })
+    }   
+    catch (error) {
         console.log(error);
     }
+}
+
+const signin = async (req, res) => {
+    try {
+        const userExists = await User.findOne({ email: req.body.email });
+        if (userExists) {
+            res.send({
+                success: false,
+                message: "The user already exists!",
+            });
+        }
+        const salt = await bcrypt.genSalt(10);
+        console.log(salt);
+        const hashPwd = bcrypt.hashSync(req.body.password, salt);
+        console.log(hashPwd);
+        req.body.password = hashPwd;
+
+
+        const newUser = await User(req.body);
+        await newUser.save();
+        res.send({
+            success: true,
+            message: "You've successfully signed up, please login now!",
+        });
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+
+
+module.exports = {
+    login,
+    signin
 }
